3ce27a217a6a32a4f10e00ef30c0842467459044
aabanbeeben <105768855+aabanbeeben@users.noreply.github.com>
2025-02-08 17:14:01 +0800

    Update filter-component.js

--- assets/js/filter-component.js
+++ assets/js/filter-component.js
@@ -5,20 +5,6 @@ class FilterComponent extends HTMLElement {
     }
 
     connectedCallback() {
-        this.shadowRoot.innerHTML = `
-            <style>
-                :host {
-                    display: block;
-                }
-            </style>
-            <select id="filter">
-                <option value="">Бүгд</option>
-                <option value="skincare">Арьс арчилгаа</option>
-                <option value="haircare">Үс арчилгаа</option>
-            </select>
-            <button id="apply-filter">Шүүлт хийх</button>
-        `;
-
         this.shadowRoot.getElementById('apply-filter').addEventListener('click', () => {
             const filterValue = this.shadowRoot.getElementById('filter').value;
             const event = new CustomEvent('filter-change', {
